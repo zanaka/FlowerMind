694c1ac21a370b60725c8ccce73d4ff015b08a95
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-01-30 17:37:13 +0900

    home topへのURLを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :bouquets
   get '/' => 'home#top'
+
+  resources :bouquets
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
