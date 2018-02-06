eaf1dcd649018373f455430bb779bedf40b7f82c
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-06 22:10:07 +0900

    linkにかざすとmessageが出るjs

---
+++ app/assets/javascripts/toolchip.js
@@ -0,0 +1,6 @@
+$(function(){
+  $(".tooltip a").hover(function() {
+    $(this).next("span").animate({opacity: "show", top: "-75"}, "slow");}, function() {
+      $(this).next("span").animate({opacity: "hide", top: "-85"}, "fast");
+    });
+});
